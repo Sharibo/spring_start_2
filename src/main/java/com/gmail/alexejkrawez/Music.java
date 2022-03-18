35e10523b249f7a55e3ec18ac3ebe66ef1d7d38f
Sharibo <AlexejKrawez@gmail.com>
2022-03-18 23:18:37 +0300

    add: spring_start_2

---
+++ src/main/java/com/gmail/alexejkrawez/Music.java
@@ -0,0 +1,5 @@
+package com.gmail.alexejkrawez;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/com/gmail/alexejkrawez/RockMusic.java
@@ -0,0 +1,8 @@
+package com.gmail.alexejkrawez;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
